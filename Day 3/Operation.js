f217dfc43f7ee477c6079ae4de88c7e6422a5696
Avinash-Raj-94 <70950859+Avinash-Raj-94@users.noreply.github.com>
2020-09-17 11:26:49 +0530

    Add files via upload

---
+++ Day 3/Operation.js
@@ -0,0 +1,22 @@
+function process(a,b) 
+{
+	var val1 = 200, val2= 800;
+	return a[b](val1,val2);
+}
+    function add(a,b)
+    {
+	return a+b
+	}
+	function sub(a,b)
+	{
+		return a-b
+	}
+	function mul(a,b)
+	{
+		return a*b
+	}
+		var opers = [add,sub,mul]
+		var oper1 = process(opers,0);
+		var oper2 = process(opers,1);
+		var oper3 = process(opers,2);
+		console.log(oper1,oper2,oper3);
